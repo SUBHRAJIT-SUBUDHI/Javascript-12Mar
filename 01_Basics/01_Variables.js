e4583dd12abf07e5b84652666edc32c6c6f4d3fd
SUBHRAJIT-SUBUDHI <132155394+SUBHRAJIT-SUBUDHI@users.noreply.github.com>
2024-03-14 03:54:16 +0000

    commit

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=1445553;
+let accountEmail="subhrajitsubudhi12345@gmail.com";
+var accountPassword="12345";
+accountCity="Bhubaneswar"
+let accountstate;
+
+//accountId=2;cannot change the value
+
+accountEmail="sipun@gmail.com";
+accountCity="jaipur"
+accountPassword="1213244";
+console.log(accountEmail);
+
+console.table([accountId , accountEmail,accountPassword,accountstate,accountCity]);
+/*
+Prefer not to use var beacuse of issue in  block scope and functional scope
+*/
