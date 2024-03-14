4ea31b1dba9d5dd1f48636f28dfa1910badfa732
SUBHRAJIT-SUBUDHI <132155394+SUBHRAJIT-SUBUDHI@users.noreply.github.com>
2024-03-14 04:04:10 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+// alert("hello"); we are using nodejs,not browser
+
+let name="sipun"//string
+let age=21//number
+let isLoggedIn=false;//boolean 
+let state;//undefined
+
+//number=>2 to pwer53
+//bigint
+//string =>""
+//boolean =>true/false
+//undefined =>typeof like  string,int etd.
+//symbol=>unique
+//null =>it is object
+
+
+//object
+
+console.log(typeof "subhrajit"); //string
+console.log(typeof  undefined);//type is undefined
+console.log(typeof null);//type is object
